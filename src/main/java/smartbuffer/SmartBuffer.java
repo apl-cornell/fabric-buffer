d4ae5cc93e90d8efad43c2a8d22b5d42abac054a
Kirill Chernyshov <kc875@cornell.edu>
2019-02-27 13:31:27 -0500

    Spaces not tabs, remove TODO, cleanup

--- src/main/java/smartbuffer/SmartBuffer.java
+++ src/main/java/smartbuffer/SmartBuffer.java
@@ -4,24 +4,21 @@ import java.util.*;
 import util.ObjectVN;
 
 public interface SmartBuffer {
-    // TODO: add method specs
-	
-	/*
-	 * Add transaction [tid] that depends on objects in [deps] to the buffer
-	 * Return true if the add is success.
-	 */
-	public boolean add(long tid, Set<ObjectVN> deps);
-	
-	/*
-	 * Remove [object] from the deps of transactions depend on [oid].
-	 * Return a list of transactions with no deps.
-	 */
-	public List<Long> remove(ObjectVN object);
-	
-	/*
-	 * Eject transactions that depends on [object] with a smaller [vnum].
-	 * Return a list of transactions ejected.
-	 */
-	public List<Long> eject(ObjectVN object);
-	
+    /*
+     * Add transaction [tid] that depends on objects in [deps] to the buffer
+     * Return true if the add is success.
+     */
+    boolean add(long tid, Set<ObjectVN> deps);
+
+    /*
+     * Remove [object] from the deps of transactions depend on [oid].
+     * Return a list of transactions with no deps.
+     */
+    List<Long> remove(ObjectVN object);
+
+    /*
+     * Eject transactions that depends on [object] with a smaller [vnum].
+     * Return a list of transactions ejected.
+     */
+    List<Long> eject(ObjectVN object);
 }
--- src/main/java/util/ObjectVN.java
+++ src/main/java/util/ObjectVN.java
@@ -1,22 +1,22 @@
 package util;
 
 public class ObjectVN {
-	/*
-	 * Unique identifier of the object
-	 */
-	public long oid;
-	
-	/*
-	 * Version number of the object
-	 */
-	public long vnum;
-	
-	public ObjectVN(long oid, long vnum){
-		this.oid = oid;
-		this.vnum = vnum;
-	}
-	
-	public boolean older(ObjectVN object) {
-		return (this.oid == object.oid && this.vnum < object.vnum);
-	}
+    /*
+     * Unique identifier of the object
+     */
+    public long oid;
+
+    /*
+     * Version number of the object
+     */
+    public long vnum;
+
+    public ObjectVN(long oid, long vnum){
+        this.oid = oid;
+        this.vnum = vnum;
+    }
+
+    public boolean older(ObjectVN object) {
+        return (this.oid == object.oid && this.vnum < object.vnum);
+    }
 }
