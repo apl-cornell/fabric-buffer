1f84dd40bc3e39d6a5aedbea7f7a40b736d8bf26
yd246 <yd246@cornell.edu>
2019-03-06 11:35:08 -0500

    Added locks in buffer

--- src/main/java/smartbuffer/NumLinkBuffer.java
+++ src/main/java/smartbuffer/NumLinkBuffer.java
@@ -10,22 +10,30 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class NumLinkBuffer implements SmartBuffer {
     /*
-     * A map from the object to transaction IDs that depend on the object
+     * A map from the object to transaction IDs that depend on the object.
      */
     private SetMultimap<ObjectVN, Long> depsMap;
 
     /*
-     * A map from a transaction ID to the number of unresolved dependencies
+     * A map from a transaction ID to the number of unresolved dependencies.
      */
     private HashMap<Long, Integer> numLink;
     
-    // map from TIDs to the futures that should resolve when all dependencies are met for that transaction
-    private HashMap<Long, Future<Boolean>> futures;
+    /*
+     * A map from oid/vnum pair to the associated lock.
+     */
+    private HashMap<ObjectVN, Lock> deplocktable;
     
-    // TODO Lock for each transaction
+    /*
+     * A map from tid to the associated lock
+     */
+    private HashMap<Long, Lock> txnlocktable;
     
     
     public NumLinkBuffer() {
@@ -33,14 +41,25 @@ public class NumLinkBuffer implements SmartBuffer {
         // Look at performance considerations, as well as whether we care about value ordering or not
         depsMap = new HashMultimap<>();
         numLink = new HashMap<>();
+        deplocktable = new HashMap<>();
+        txnlocktable = new HashMap<>();
     }
 
     @Override
     public void add(long tid, Set<ObjectVN> deps) {
-        for (ObjectVN object : deps) {
-            depsMap.put(object, tid);
+        txnlocktable.put(tid, new ReentrantLock());
+        synchronized(txnlocktable.get(tid)){
+            for (ObjectVN object : deps) {
+                if (!deplocktable.containsKey(object)) {
+                    deplocktable.put(object, new ReentrantLock());
+                }
+                // How setmultimap is implemented?
+                synchronized(txnlocktable.get(object)) {
+                    depsMap.put(object, tid);
+                }
+            }
+            numLink.put(tid, deps.size());
         }
-        numLink.put(tid, deps.size());
     }
 
     @Override
@@ -48,9 +67,11 @@ public class NumLinkBuffer implements SmartBuffer {
         List<Long> translist = new LinkedList<Long>();
         for (long tid : depsMap.get(object)) {
             if (numLink.containsKey(tid)) {
-                numLink.put(tid, numLink.get(tid) - 1);
-                if (numLink.get(tid) == 0) {
-                    translist.add(tid);
+                synchronized(txnlocktable.get(tid)) {
+                    numLink.put(tid, numLink.get(tid) - 1);
+                    if (numLink.get(tid) == 0) {
+                        translist.add(tid);
+                    }
                 }
             }
         }
@@ -64,8 +85,10 @@ public class NumLinkBuffer implements SmartBuffer {
         for (ObjectVN object_curr : depsMap.keySet()) {
             if (object_curr.older(object)) {
                 for (long tid : depsMap.get(object_curr)) {
-                    translist.add(tid);
-                    numLink.remove(tid);
+                    synchronized(txnlocktable.get(tid)){
+                        translist.add(tid);
+                        numLink.remove(tid);
+                    }
                 }
                 depsMap.removeAll(object_curr);
             }
