e9a8307743b3f720409771fab5cb258d79f29ddf
yd246 <yd246@cornell.edu>
2019-03-06 09:41:57 -0500

    Added locks on the store's side

--- src/main/java/util/StoreSB.java
+++ src/main/java/util/StoreSB.java
@@ -1,8 +1,12 @@
 package util;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
+import java.util.concurrent.locks.ReadWriteLock;
 
 import com.google.common.collect.SetMultimap;
 
@@ -31,11 +35,17 @@ public class StoreSB implements Store {
      */
     public HashMap<Long, CompletableFuture<Boolean>> futures;
     
+    /*
+     * A map from [oid] to RWlock.
+     */
+    public HashMap<Long, ReadWriteLock> locktable;
+    
     public StoreSB(SmartBuffer buffer) {
         this.buffer = buffer;
         this.lastversion = new HashMap<>();
         this.pending = new SetMultimap<>();
         this.pendingread = new SetMultimap<>();
+        this.locktable = new HashMap<>();
     }
     
     @Override
@@ -55,11 +65,13 @@ public class StoreSB implements Store {
         
         pending.putAll(tid, writes);
         if (actualdeps.isEmpty()) {
-            // TODO Grab the lock on the store's side
-            
-            // TODO Change the state of [tid] to Prepared
-            
-            return futurewith(true);
+            // Grab the lock on the store's side
+            if (!grablock(reads,writes)) {
+                releaselock(reads,writes);
+                return futurewith(false);
+            } else {
+                return futurewith(true);
+            }  
         } else {
             // Result resolved to true if the dependencies of [tid] are resolved. resolved to false only when there is version conflict
             buffer.add(tid, actualdeps);
@@ -67,9 +79,13 @@ public class StoreSB implements Store {
             actualdeps = depscheck(reads);
             if (actualdeps.isEmpty()) {
                 buffer.delete(tid);
-                // Grab the lock and change the state of [tid]
-                
-                return futurewith(true);
+             // Grab the lock on the store's side
+                if (!grablock(reads,writes)) {
+                    releaselock(reads,writes);
+                    return futurewith(false);
+                } else {
+                    return futurewith(true);
+                } 
             } else {
                 // Add a result to be filled to the futures. 
                 // Resolve [result] when the transaction is prepared successfully or ejected because of version conflict
@@ -95,10 +111,12 @@ public class StoreSB implements Store {
     
     @Override
     public void commit(long tid) {
-    	for (ObjectVN object : pending.get(tid)) {
-    	    lastversion.put(object.oid, object.vnum);
+    	for (ObjectVN write : pending.get(tid)) {
+    	    lastversion.put(write.oid, write.vnum);
+    	    //Release Lock
+    	    releaselock(pendingread.get(tid), pending.get(tid));
     	    //Remove object from the buffer
-    	    List<Long> translist = buffer.remove(object);
+    	    List<Long> translist = buffer.remove(write);
             //Prepare objects that have all dependencies removed
     	    for (long tid1 : translist) {
     	        //Check version conflict
@@ -110,12 +128,20 @@ public class StoreSB implements Store {
     	            }
     	        }
     	        if (novc) {
-    	            futures.get(tid1).complete(true);
+    	         // Grab the lock on the store's side
+    	            Set<ObjectVN> reads = pendingread.get(tid1);
+    	            Set<ObjectVN> writes = pending.get(tid1);
+    	            if (!grablock(reads, writes)) {
+    	                releaselock(reads,writes);
+    	                futures.get(tid1).complete(false);
+    	            } else {
+    	                futures.get(tid1).complete(true);
+    	            } 
     	        }
     	        futures.remove(tid1);
     	    }
     	    //Abort transactions with version conflict to this commit
-    	    List<Long> ejectlist = buffer.eject(object);
+    	    List<Long> ejectlist = buffer.eject(write);
     	    for (long tid1 : ejectlist) {
     	        abort(tid1);
     	    }
@@ -136,6 +162,35 @@ public class StoreSB implements Store {
 	
 	private <T> Future<T> futurewith(T value){
 	    return CompletableFuture.completedFuture(value);
+	}
+	
+	/*
+	 * Grab the lock for objects in reads and writes.
+	 */
+	private boolean grablock(Set<ObjectVN> reads, Set<ObjectVN> writes) {
+	    for (ObjectVN read : reads) {
+	        if (!locktable.get(read.oid).readLock().tryLock()) {
+	            return false;
+	        }
+	    }
+	    for (ObjectVN write : writes) {
+	        if (!locktable.get(write.oid).writeLock().tryLock()) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+	
+	/*
+	 * Release the lock for objects in reads and writes.
+	 */
+	private void releaselock(Set<ObjectVN> reads, Set<ObjectVN> writes) {
+	    for (ObjectVN read : reads) {
+	        locktable.get(read.oid).readLock().unlock();
+	    }
+	    for (ObjectVN write : writes) {
+	        locktable.get(write.oid).writeLock().unlock();
+	    }
 	}
 
 }
