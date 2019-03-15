bc98cb3fd548c471ce8c6c11b8c135580920e8c1
yd246 <yd246@cornell.edu>
2019-03-15 14:24:07 -0400

    Added Uniform/Gaussian in TxnGenerator

--- src/main/java/util/TxnGenerator.java
+++ src/main/java/util/TxnGenerator.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -19,6 +23,15 @@ public class TxnGenerator {
     /*The biggest unused tid */
     private long tid;
     
+    /*Distribution for transaction*/
+    ProbDis probtype;
+    
+    /*number of reads in a transaction*/
+    private int readsize;
+    
+    /*number of writes in a transaction*/
+    private int writesize;
+    
     /* Generate a new unique tid */
     private long generateTid() {
         tid++;
@@ -26,8 +39,8 @@ public class TxnGenerator {
     }
     
     /* Ratio of read object and write object */
-    private static double readsratio;
-    private static double writesratio;
+    private double numObjectratio;
+    private double rwratio;
 
     private static final int TXN_QUEUE_CAPACITY = 10;
 
@@ -37,27 +50,80 @@ public class TxnGenerator {
         this.queue = new ArrayBlockingQueue<>(TXN_QUEUE_CAPACITY);
     }
     
+    /* Construct a txnGenerator for fixed size transactions  */
+    public TxnGenerator(Worker worker, int readsize, int writesize) {
+        this.worker = worker;
+        this.wid = worker.wid;
+        this.queue = new ArrayBlockingQueue<>(TXN_QUEUE_CAPACITY);
+        this.probtype = ProbDis.FixedSize;
+        this.readsize = readsize;
+        this.writesize = writesize;
+    }
+    
+    /**/
+    public TxnGenerator(Worker worker, ProbDis probtype, double numObjectratio, double rwratio) {
+        this.worker = worker;
+        this.wid = worker.wid;
+        this.queue = new ArrayBlockingQueue<>(TXN_QUEUE_CAPACITY);
+        this.probtype = probtype;
+        this.numObjectratio = numObjectratio;
+        this.rwratio = rwratio;
+    }
+    
     /* Generate a new transaction */
     private void newTxn() {
         SetMultimap<Store, ObjectVN> reads = new HashMultimap<>();
         SetMultimap<Store, ObjectVN> writes = new HashMultimap<>();
         
         // Randomly generate reads
-        for (long oid : worker.lastversion.keySet()) {
-            double random = Math.random();
-            if (random > readsratio) {
-                reads.put(worker.location.get(oid), 
-                        new ObjectVN(oid, worker.lastversion.get(oid)));
-            }
-        }
-        
-        // Pick writes from reads
-        for (ObjectVN object : reads.values()) {
-            double random = Math.random();
-            if (random > writesratio) {
-                writes.put(worker.location.get(object.oid), 
-                        new ObjectVN(object.oid, object.vnum + 1));
-            }
+        switch(probtype) {
+            case FixedSize:
+                // TODO : Any better way to do this?
+                // Generate sorted distinct random index
+                int size = worker.lastversion.keySet().size();
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                for (int i = 0; i < size; i ++) {
+                    list.add(new Integer(i));
+                }
+                Collections.shuffle(list);
+                LinkedList<Integer> rwlist = new LinkedList<Integer>();
+                for (int i = 0; i < readsize + writesize; i++) {
+                    rwlist.add(list.get(i));
+                }
+                Collections.sort(rwlist);
+                
+                //Pick items from worker.lastversion based on rwlist
+                
+                
+            case Uniform: 
+                for (long oid : worker.lastversion.keySet()) {
+                    Random rand = new Random();
+                    double random = rand.nextDouble();
+                    if (random < numObjectratio) {
+                        random = Math.random();
+                        if (random < rwratio) {
+                            writes.put(worker.location.get(oid),
+                                    new ObjectVN(oid, worker.lastversion.get(oid) + 1));
+                        }
+                        reads.put(worker.location.get(oid), 
+                                new ObjectVN(oid, worker.lastversion.get(oid)));
+                    }
+                }
+            
+            case Gaussian:
+                for (long oid : worker.lastversion.keySet()) {
+                    Random rand = new Random();
+                    double random = rand.nextGaussian() + 0.5;
+                    if (random < numObjectratio) {
+                        random = Math.random();
+                        if (random < rwratio) {
+                            writes.put(worker.location.get(oid),
+                                    new ObjectVN(oid, worker.lastversion.get(oid) + 1));
+                        }
+                        reads.put(worker.location.get(oid), 
+                                new ObjectVN(oid, worker.lastversion.get(oid)));
+                    }
+                }
         }
         
         long ntid = generateTid();
@@ -68,5 +134,10 @@ public class TxnGenerator {
         } catch (InterruptedException e) {
             // TODO: should we handle this somehow?
         }
+    }
+    
+    /*type of probability distribution*/
+    public enum ProbDis {
+        FixedSize, Uniform, Gaussian
     }
 }
