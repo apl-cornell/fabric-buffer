8c2f40ae8d66527876ce37281aaa252cfb7b4cd0
Kirill Chernyshov <kc875@cornell.edu>
2019-03-15 13:45:02 -0400

    Upgrade to Junit5, add test file for SmartBuffer

---
+++ src/test/java/smartbuffer/SmartBufferTest.java
@@ -0,0 +1,17 @@
+package smartbuffer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmartBufferTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
